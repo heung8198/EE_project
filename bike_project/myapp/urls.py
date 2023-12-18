cfff96d132f32fcaccc313e5bf70dbdb40cc9e96
J-W-Kim-GitHub <82186213+J-W-Kim-GitHub@users.noreply.github.com>
2023-12-18 21:44:56 +0900

    last

--- bike_project/myapp/urls.py
+++ bike_project/myapp/urls.py
@@ -5,9 +5,9 @@ from myapp.views import fetch_bike_data, bike_station_shortages, bike_station_pr
 from . import views
 urlpatterns = [
     # path('bike-stations/', save_data, name='save_data'),
-    path('bike-stations/', bike_station_shortages, name='bike_station_shortages'),
-    path('bike-stations/', bike_station_predictions, name='bike_station_predictions'),
-    path('bike-stations/', fetch_bike_data_view, name='fetch_bike_data_view'),
+    path('', bike_station_shortages, name='bike_station_shortages'),
+    path('', bike_station_predictions, name='bike_station_predictions'),
+    path('', fetch_bike_data_view, name='fetch_bike_data_view'),
     path('fetch-learning-data/', fetch_learning_data, name='fetch_learning_data'),
     path('fetch-bike-data/', fetch_bike_data, name='fetch_bike_data'),
     path('fetch-weather-data/', fetch_weather_data, name='fetch_weather_data'),
